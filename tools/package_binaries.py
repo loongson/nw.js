170040ddfea43ba270a63c58204d08f2cc1be3ba
wjh-la <wujiahuan@loongson.cn>
2023-03-17 10:29:28 +0800

    Add loongarch64 support for nwjs build

--- tools/package_binaries.py
+++ tools/package_binaries.py
@@ -155,22 +155,12 @@ def generate_target_nw(platform_name, arch, version):
                            'lib/libEGL.so',
                            'lib/libGLESv2.so',
                            'lib/libvulkan.so.1',
-                           'lib/libvk_swiftshader.so',
-                           'lib/vk_swiftshader_icd.json'
                            ]
         if flavor == 'sdk':
             target['input'].append('nwjc')
             target['input'].append('payload')
             target['input'].append('chromedriver')
             target['input'].append('minidump_stackwalk')
-        if flavor in ['nacl','sdk'] :
-            target['input'] += ['nacl_helper', 'nacl_helper_bootstrap', 'pnacl']
-            if arch == 'x64':
-                target['input'].append('nacl_irt_x86_64.nexe')
-            elif arch == 'ia32':
-                target['input'].append('nacl_irt_x86_32.nexe')
-            else:
-                target['input'].append('nacl_irt_{}.nexe'.format(arch))
             
     elif platform_name == 'win':
         target['input'] = [
@@ -451,8 +441,6 @@ def make_packages(targets):
             os.mkdir(folder)
             if platform_name == 'linux':
                 os.mkdir(os.path.join(folder, 'lib'))
-            if platform_name in ['linux', 'win']:
-                os.mkdir(os.path.join(folder, 'swiftshader'))
             for f in t['input']:
                 src = os.path.join(binaries_location, f)
                 dest = os.path.join(folder, f)
